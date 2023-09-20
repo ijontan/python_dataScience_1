8b4ef757ecca572dda0fe595e831a919e08fdeea
ijon <itan@student.42kl.edu.my>
2023-09-20 21:12:09 +0800

    ex02 done

---
+++ ex02/load_image.py
@@ -0,0 +1,14 @@
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str) -> np.array:
+    """ft_load"""
+    try:
+        assert isinstance(path, str), "path must be a string"
+        img = Image.open(path)
+        img_arr = np.array(img)
+        print("The shape of image is :", img_arr.shape)
+        return img_arr
+    except Exception as e:
+        print("Exception: ", e)
